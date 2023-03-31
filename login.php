6dbfffbfa86698682aa18d87fd49f1756c4bb9d9
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-03-31 13:57:48 +0200

    add login adn register

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label>Email:</label>
+        <input type="email" name="email"><br>
+        <label>Password:</label>
+        <input type="password" name="password"><br>
+        <input type="submit" name="login" value="Login!">
+    </form>
+</body>
+</html>
---
+++ registrace.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrace</title>
+    <script
+    src="https://code.jquery.com/jquery-3.6.3.min.js"
+    integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+    crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        print_r($_POST);
+    ?>
+    <form action="" method="post">
+
+        <label>Email:</label>
+        <input type="email" name="email"><br>
+
+        <label>Password:</label>
+        <input type="password" name="password"><br>
+        
+        <label>Confirm Password:</label>
+        <input type="password" name="check_password"><br>
+
+        <input type="submit" name="register" value="Register!">
+
+    </form>
+    <script>
+        $("form").submit(isFormValid);
+
+        function isFormValid(event)
+        {
+            $(".error").remove();   
+            isInputFilled("email");  
+            isInputFilled("password");         
+            isInputFilled("check_password");                      
+            
+            //Když jsou chyby tak nepokračuj
+            if($(".error").length > 0){
+                event.preventDefault();
+                return;
+            }
+            $("form").unbind("submit");
+
+        }    
+        function isInputFilled(inputName){
+            let input = $("input[name="+inputName+"]");
+            if(input.val().trim() == "")
+                input.after("<span class='error'>Chybí povinný údaj! Blbče!</span>")
+
+        }
+</script>
+</body>
+</html>
