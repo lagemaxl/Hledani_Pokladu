4290f04af06bbdb052e4ed2de8123da20bf2879b
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-04-02 19:21:36 +0200

    add files for game

---
+++ game.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hledání Pokladu</title>
+    <link rel="stylesheet" href="styly.css">
+</head>
+<body>
+    <div class="poleLength">
+        <form action="">
+            <label for="poleLength">Zadejte velikost pole:</label>
+            <input type="number" name="poleLengthX" id="poleLengthX" min="5" max="20" value="10">
+            <input type="number" name="poleLengthY" id="poleLengthY" min="5" max="20" value="10">
+            <input type="submit" value="Zvolit velikost">
+        </form>    
+    </div>
+
+
+
+    <div class="container">
+
+    </div>
+
+
+
+
+    <script src="game.js"></script>
+</body>
+</html>
