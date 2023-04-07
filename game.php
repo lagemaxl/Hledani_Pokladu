fed052327eb72370a280ace631a2db3fc3788a72
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-04-07 11:21:46 +0200

    create arry 500x500

--- game.php
+++ game.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hledání Pokladu</title>
-    <link rel="stylesheet" href="style/styly.css">
+    <link rel="stylesheet" href="style/game.css">
 </head>
 <body>
     <div class="poleLength">
-        <form action="">
+        <form>
             <label for="poleLength">Zadejte velikost pole:</label>
             <input type="number" name="poleLengthX" id="poleLengthX" min="5" max="20" value="10">
             <input type="number" name="poleLengthY" id="poleLengthY" min="5" max="20" value="10">
@@ -19,7 +19,7 @@
 
 
 
-    <div class="container">
+    <div class="gameArea">
 
     </div>
 
