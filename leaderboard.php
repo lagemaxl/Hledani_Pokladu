93ba1bb57ab876f6d6f28b23629c595362f0c7b7
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-04-16 13:40:54 +0200

    leaderboard works

--- leaderboard.php
+++ leaderboard.php
@@ -8,11 +8,23 @@
 </head>
 <body>
     <?php 
+    include 'connection.php';
     session_start();
-    echo $_SESSION['user_id'];
+    //echo $_SESSION['user_id']; //kontrola zda se id uživatele přenáší
     
-    
-    
+    $sql = "SELECT * FROM leaderboard WHERE id_user = {$_SESSION['user_id']}";
+    $result = $connect->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<th>PoleX</th><th>PoleY</th><th>Clicks</th></tr>";
+        while($row = $result->fetch_assoc()) {
+            echo "<td>".$row["poleX"]."</td><td>".$row["poleY"]."</td><td>".$row["clicks"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Zatím žádné záznamy";
+    }
     ?>
 </body>
 </html>
